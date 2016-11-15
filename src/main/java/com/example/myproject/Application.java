fb7868e556572a17b404fa6bda1b53a6d71d41a9
wangweiquan <Administrator@PC-20160606TZLO>
2016-11-15 14:54:48 +0800

    commit change

--- src/main/java/com/example/myproject/Application.java
+++ src/main/java/com/example/myproject/Application.java
@@ -1,23 +1,20 @@
 package com.example.myproject;
 
-import javafx.scene.Parent;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-/*@Configuration
+@Configuration
 @EnableAutoConfiguration
-@ComponentScan  //用来搜索应用程序组件,包含直接(@Component,@Service,@Repository,@Controller)
-//使用@ComponentScan 搜索beans 并且结合@Autowired构造器注入依赖
+@ComponentScan
+//用来搜索应用程序组件,包含直接(@Component,@Service,@Repository,@Controller)   使用@ComponentScan 搜索beans 并且结合@Autowired构造器注入依赖
 public class Application {
 	public static void main(String [] args) {
 		SpringApplication.run(Application.class, args);
 	}
-}*/
+}
 
 
 /*
